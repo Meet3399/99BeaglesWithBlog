b147d373c45d97d1c2c47643a260bfc09aeec440
MM3399 <techteam1@thebrowser.in>
2020-11-21 11:21:16 +0530

    Navigation bar coming from wagtail

--- 99Beagles/src/components/Nav.svelte
+++ 99Beagles/src/components/Nav.svelte
@@ -1,19 +1,26 @@
+
 <script>
 	export let segment;
-	  import {token , userName} from '../routes/store';
-  let jwt;
-  let name;
-	token.subscribe(value=>{
-		jwt = value
-	})
-	userName.subscribe(value=>{
-		name = value
-	})
+	import { token, userName } from "../routes/store";
+	let jwt;
+	let name;
+	export let navItems;
+	
+	token.subscribe((value) => {
+		jwt = value;
+	});
+	userName.subscribe((value) => {
+		name = value;
+	});
+
+	
+
 </script>
 
 <style>
+	@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
+		border-bottom: 1px solid rgba(255, 62, 0, 0.1);
 		font-weight: 300;
 		padding: 0 1em;
 	}
@@ -25,7 +32,7 @@
 
 	/* clearfix */
 	ul::after {
-		content: '';
+		content: "";
 		display: block;
 		clear: both;
 	}
@@ -40,12 +47,28 @@
 		display: inline-block;
 	}
 
-	[aria-current]::after {
-		position: absolute;
-		content: '';
+	select{
+		/* position: absolute;
+		content: "";
 		width: calc(100% - 1em);
 		height: 2px;
-		background-color: rgb(255,62,0);
+		background-color: rgb(255, 62, 0);
+		display: block; */
+		/* bottom: -1px; */
+		border: none;
+		width: 100px;
+		position: relative;
+		top:15px;
+		margin: 5px;
+
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: "";
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: rgb(255, 62, 0);
 		display: block;
 		bottom: -1px;
 	}
@@ -65,18 +88,83 @@
 
 <nav>
 	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">Blog</a></li>
+		{#each navItems as nav,index }
+			
+				<!-- {console.log(index,nav)} -->
+			
+				{#if !Array.isArray(nav)}
+					{#if nav.navItem === 'Login'}
+						{#if !jwt}
+							<!-- {console.log("Coming in if block")} -->
+							<li>
+							<a aria-current={segment === nav.current ? 'page' : undefined}
+							href={nav.navLink}>{nav.navItem}</a>
+							</li>
+						{/if}
+					{:else if nav.navItem === 'Logout'}
+						{#if jwt}
+							<!-- {console.log("Coming in else if block")} -->
+							<li>
+								<a aria-current={segment === nav.current ? 'page' : undefined}
+								href={nav.navLink}>{nav.navItem}</a>
+							</li>
+						{/if}
+					{:else}
+						<!-- {console.log("Coming in else block")} -->
+						<li>
+							<a aria-current={segment === nav.current ? 'page' : undefined}
+							href={nav.navLink}>{nav.navItem}</a>
+						</li>
+					{/if}
+				{:else}
+					<li>
+						<select>
+							{#each nav as n}
+								<option>{n.navItem}</option>
+							{/each}
+						</select>
+					</li>
+					<!-- <select>
+						{#each nav as navOptions}
+							{#if navOptions.subnav}
+								<option>
+									<a href={navOptions.navLink}>{navOptions.navItem}</a>
+								</option>
+							{:else}
+								<option>{navOptions.navItem}</option>
+							{/if}
+						{/each} 
+					</select>						 -->
+				{/if}
+			{/each}
+		<!-- <li>
+			<a
+				aria-current={segment === undefined ? 'page' : undefined}
+				href="/">Home</a>
+		</li>
+		<li>
+			<a
+				aria-current={segment === 'about' ? 'page' : undefined}
+				href="/about">Blog</a>
+		</li>
 		{#if !jwt}
-		<li><a aria-current="{segment === 'login' ? 'page' : undefined}" href="login">Login</a></li>
+			<li>
+				<a
+					aria-current={segment === 'login' ? 'page' : undefined}
+					href="/login">Login</a>
+			</li>
 		{:else}
-		<li><a aria-current="{segment === 'logout' ? 'page' : undefined}" href="logout">Logout</a></li>
-		{/if}
+			<li>
+				<a
+					aria-current={segment === 'logout' ? 'page' : undefined}
+					href="/logout">Logout</a>
+			</li>
+		{/if} -->
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<!-- <li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li> -->
-	
+
 		{#if jwt}
 			<p>Hey, {name}</p>
 		{/if}
--- 99Beagles/src/routes/_layout.svelte
+++ 99Beagles/src/routes/_layout.svelte
@@ -1,6 +1,46 @@
+<script context="module">
+export async function preload(){
+	// const start = new Date().getTime();
+	const proxyUrl = "https://still-taiga-46464.herokuapp.com/";
+	const response  = await this.fetch ('https://www.99beagles.com/api/v2/menus/')
+	
+	const data = await response.json();
+	const navItems = []
+	// console.log(data , new Date().getTime() - start , "ms");
+	for (let i = 0; i < data[0].menu.length; i++) {
+			if (data[0].menu[i].type === "nav_category") {
+				const navCategoryData = [{
+					navItem: data[0].menu[i].value.title,
+					hasSubNav: true,
+					current: 'title'
+				}]
+				for(let j=0;j<data[0].menu[i].value.sub_nav.length;j++){
+					const navBarData = {
+						navItem: data[0].menu[i].value.sub_nav[j].value.override_title,
+						navLink: data[0].menu[i].value.sub_nav[j].value.link,
+						subNav: true,
+					}
+					navCategoryData.push(navBarData);
+				}
+				navItems.push(navCategoryData)
+			} else {
+				const navBarData = {
+					navItem: data[0].menu[i].value.override_title,
+					current: data[0].menu[i].value.override_title.toLowerCase(),
+					navLink: data[0].menu[i].value.link,
+					hasSubNav:  false
+				};
+				navItems.push(navBarData);
+			}
+		}
+		// 	
+	return {navItems}
+}
+</script>
+
 <script>
 	import Nav from '../components/Nav.svelte';
-
+	export let navItems
 	export let segment;
 </script>
 
@@ -15,7 +55,7 @@
 	}
 </style>
 
-<Nav {segment}/>
+<Nav {segment}{navItems} />
 
 <main>
 	<slot></slot>
